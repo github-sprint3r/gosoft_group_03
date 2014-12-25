a47cd0d1cb043be278641f25413ca73e3ffdcb05
marukojung991 <marukojung_991@hotmail.com>
2014-12-25 14:33:38 +0700

    add get data from table Park_Transaction, DB ParkKoDB

--- JavaSource/ParkKoUtil/src/bean/User.java
+++ JavaSource/ParkKoUtil/src/bean/User.java
@@ -100,6 +100,34 @@ public class User {
 			return false;
 		return true;
 	}
+	
+	public boolean equalsKey(Object obj){
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (carId == null) {
+			if (other.carId != null)
+				return false;
+		} else if (!carId.equals(other.carId))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		
+		if (province == null) {
+			if (other.province != null)
+				return false;
+		} else if (!province.equals(other.province))
+			return false;
+		
+		return true;
+	}
 
 	@Override
 	public String toString() {
