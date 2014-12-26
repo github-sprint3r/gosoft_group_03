a454e5803972c92d0d97cc799d512a0f253243fe
thanaponcha <thanaponcha@THANAPONCHA.7eleven.cp.co.th>
2014-12-26 10:30:15 +0700

    add function calculateFee

---
+++ JavaSource/ParkKoUtil/src/business/TestCalculateFee.java
@@ -0,0 +1,23 @@
+package business;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCalculateFee {
+
+	CalculateFee calculateFee = new CalculateFee();
+	@Test
+	public void seven_hours_in_normal_case_and_three_hours_in_abnormal_case_should_fees_equal_840_baht() {
+		int expectedResult = 840;
+		int actualResult = calculateFee.calculateFee(7, 3);
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void six_hours_in_normal_case_should_fees_equal_70_baht() {
+		int expectedResult = 70;
+		int actualResult = calculateFee.calculateFee(6, 0);
+		assertEquals(expectedResult, actualResult);
+	}
+}
