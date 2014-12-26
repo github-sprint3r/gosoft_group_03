b23bfe5a1e9fce50744638ecc68062ebf24da7d1
thanaponcha <thanaponcha@THANAPONCHA.7eleven.cp.co.th>
2014-12-26 09:17:37 +0700

    add class for convert arabic number in string to thai number

---
+++ JavaSource/ParkKoUtil/src/business/ConvertType.java
@@ -0,0 +1,22 @@
+package business;
+
+public class ConvertType {
+
+	public String convertCarIdWithArabicToCarIdWithThaiNumber(String carId){
+		char[] thaiChars = {'๐','๑','๒','๓','๔','๕','๖','๗','๘','๙'};
+		StringBuilder carIdWithThaiNumber = new StringBuilder();
+		
+		for(int index =0;index<carId.length();index++)
+		{
+		    if(Character.isDigit(carId.charAt(index)))
+		    {
+		    	carIdWithThaiNumber.append(thaiChars[(int)(carId.charAt(index))-48]);
+		    }
+		    else
+		    {
+		    	carIdWithThaiNumber.append(carId.charAt(index));
+		    }
+		}
+		return carIdWithThaiNumber.toString();
+	}
+}
